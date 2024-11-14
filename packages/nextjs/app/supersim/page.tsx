0cc21c11feb440ee53608fbe617c28e081eaeee3
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2024-11-14 12:58:04 -0500

    feat: bridge from l2 to l2

--- packages/nextjs/app/supersim/page.tsx
+++ packages/nextjs/app/supersim/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { EthIcon } from "./_assets/icons";
 import { NextPage } from "next";
 import { formatEther, parseEther } from "viem";
 import { useAccount, useBalance, useReadContract, useWalletClient } from "wagmi";
@@ -65,9 +66,42 @@ const Supersim: NextPage = () => {
     },
   ];
 
+  const l2Erc20BridgeAbi = [
+    {
+      inputs: [
+        {
+          internalType: "address",
+          name: "_token",
+          type: "address",
+        },
+        {
+          internalType: "address",
+          name: "_to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "_amount",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "_chainId",
+          type: "uint256",
+        },
+      ],
+      name: "sendERC20",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+
   const l1StandardBridgeAddress = L1Chain.contracts.L1StandardBridgeProxy.address;
 
   const L2NativeSuperchainERC20Address = "0x420beeF000000000000000000000000000000001";
+
+  const L2Erc20BridgeAddress = "0x4200000000000000000000000000000000000028";
 
   const { address: connectedAddress } = useAccount();
 
@@ -113,6 +147,26 @@ const Supersim: NextPage = () => {
     console.log(txResponse);
   };
 
+  const handleBridgeErc20ToL2 = async () => {
+    const bridgeData = writeContract({
+      abi: l2Erc20BridgeAbi,
+      address: L2Erc20BridgeAddress,
+      functionName: "sendERC20",
+      args: [L2NativeSuperchainERC20Address, connectedAddress, 1000, 902],
+      chainId: L2ChainA.id,
+    });
+
+    const tx = {
+      from: connectedAddress,
+      to: L2Erc20BridgeAddress,
+      data: bridgeData,
+    };
+
+    //@ts-ignore
+    const txResponse = await walletClient?.sendTransaction(tx);
+    console.log(txResponse);
+  };
+
   const { data: l1Balance, isLoading: l1BalanceIsLoading } = useBalance({
     address: connectedAddress,
     chainId: L1Chain.id,
@@ -136,6 +190,13 @@ const Supersim: NextPage = () => {
     chainId: L2ChainA.id,
   });
 
+  const { data: chainBErc20Balance, isLoading: chainBErc20IsLoading } = useReadContract({
+    address: L2NativeSuperchainERC20Address,
+    abi: L2NativeSuperchainERC20ABI,
+    functionName: "balanceOf",
+    args: [connectedAddress],
+    chainId: L2ChainB.id,
+  });
   return (
     <>
       <h1>Hello, world!</h1>
@@ -144,6 +205,9 @@ const Supersim: NextPage = () => {
       </button>
       <button className="btn" onClick={handleMintToL2}>
         Mint ERC20 on L2 OP Chain A
+      </button>
+      <button className="btn" onClick={handleBridgeErc20ToL2}>
+        Bridge ERC20 from OP Chain A to OP Chain B
       </button>
       <div>
         <h1>Account Balances</h1>
@@ -154,7 +218,7 @@ const Supersim: NextPage = () => {
             <p>Loading...</p>
           ) : (
             <p>
-              {l1Balance?.formatted} {l1Balance?.symbol}
+              {l1Balance?.formatted} {EthIcon}
             </p>
           )}
         </div>
@@ -164,7 +228,7 @@ const Supersim: NextPage = () => {
             <p>Loading...</p>
           ) : (
             <p>
-              {l2ChainABalance?.formatted} {l2ChainABalance?.symbol}
+              {l2ChainABalance?.formatted} {EthIcon}
             </p>
           )}
         </div>
@@ -181,6 +245,10 @@ const Supersim: NextPage = () => {
         <div>
           <h2>L2 Chain A Tokens:</h2>
           {chainAErc20IsLoading ? <p>Loading...</p> : <p>{Number(chainAErc20Balance)}</p>}
+        </div>
+        <div>
+          <h2>L2 Chain B Tokens:</h2>
+          {chainBErc20IsLoading ? <p>Loading...</p> : <p>{Number(chainBErc20Balance)}</p>}
         </div>
       </div>
     </>
